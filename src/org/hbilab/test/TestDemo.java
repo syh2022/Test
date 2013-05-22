7fdf3c6ba0ddbbbebc463399a99608dfaa3e65cf
syh1011 <syh@hbilab.org>
2013-05-22 18:12:32 +0900

    TestDemo.java 파일생성

---
+++ src/org/hbilab/test/TestDemo.java
@@ -0,0 +1,13 @@
+package org.hbilab.test;
+
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TestDemo");
+	}
+
+}
